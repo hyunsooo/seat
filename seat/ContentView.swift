df1f142920e35aab7db99a1f14f18cdcd01c2e42
hyunsoo_han <hyunsu_han@fursys.com>
2019-10-15 09:52:16 +0900

    Initial Commit

---
+++ seat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  seat
+//
+//  Created by FURSYS on 2019/10/15.
+//  Copyright © 2019 FURSYS. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
